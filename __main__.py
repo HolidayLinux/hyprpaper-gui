c51f7c0ccaae441a1a487cdac43f9d56435a0882
holiday <alega00@mail.ru>
2025-02-24 08:06:58 +0800

    add config.json file

--- __main__.py
+++ __main__.py
@@ -1,12 +1,18 @@
+import json
 import flet as ft
 from pathlib import Path
 from icon_creater import get_files_in_directory
 from imagewrapper import ImageWrapper
-from progressbar import ProgressBar
 
+def load_config():
+    with open('config.json') as config_json:
+        config = json.load(config_json)
+        return config['wallpaper_directory']
 
+        
+        
 def main(page: ft.Page):
-    wallpaper_directory = 'c:\\Users\\Holiday\\.dotfiles\\wallpapers\\Wallpaper-Bank'
+    wallpaper_directory = load_config() 
     page.title = "HYPRGUIPAPER"
     page.theme_mode = ft.ThemeMode.DARK
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
@@ -15,6 +21,5 @@ def main(page: ft.Page):
 
     page.add(ft.Row(controls=new_controls, wrap=True,
                     scroll=ft.ScrollMode.AUTO, expand=True))
-
 
 ft.app(target=main)
