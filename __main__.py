5d5be162bb253d04b94d06890236c042437b97ae
holiday <alega00@mail.ru>
2025-02-27 13:15:32 +0800

    add imagebutton, worked load

--- __main__.py
+++ __main__.py
@@ -1,49 +1,53 @@
-import asyncio
 import json
 from pathlib import Path
 
 import flet as ft
-from PIL.Image import Image
 
+from fletimgbtn import ImageButton
 from icon_creater import (
     create_cached_image_mem,
     get_files_in_directory,
     image_to_base64,
 )
-from imagewrapper import ImageWrapper
 
 
-def load_config():
+def load_config(param: str):
     with open("config.json") as config_json:
         config = json.load(config_json)
-        return config["wallpaper_directory"]
+        return config[param]
 
 
 def main(page: ft.Page):
-    wallpaper_directory = load_config()
+    wallpaper_directory = load_config("wallpaper_directory")
     page.title = "HYPRGUIPAPER"
     page.theme_mode = ft.ThemeMode.DARK
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     wallpaper_paths = get_files_in_directory(Path(wallpaper_directory))
+
+    image_buttons = [
+        ImageButton(image_path=load_config("default_wallpaper"))
+        for _ in range(0, len(wallpaper_paths))
+    ]
+
     images = [
-        ft.Image(src="./assets/images/loading2.gif")
+        ft.Image(src=load_config("default_wallpaper"))
         for _ in range(0, len(wallpaper_paths))
     ]
     old_controls = [ft.TextButton(content=image) for image in images]
     image_row = ft.Row(
-        controls=old_controls, wrap=True, scroll=ft.ScrollMode.AUTO, expand=True
+        controls=image_buttons, wrap=True, scroll=ft.ScrollMode.AUTO, expand=True
     )
 
-    async def feel_image(image: ft.Image, cached_wallpaper: Path):
+    async def feel_image(image: ImageButton, cached_wallpaper: Path):
         cached_image = await create_cached_image_mem(cached_wallpaper)
-        image.src_base64 = image_to_base64(cached_image)
+        image.change_image(image_to_base64(cached_image))
         print(f"end of work : {cached_wallpaper}")
         if image.page != None:
             image.page.update()
 
     async def create_cached_images():
         create_images = [
-            feel_image(images[x], image_path)
+            feel_image(image_buttons[x], image_path)
             for x, image_path in enumerate(wallpaper_paths)
         ]
 
--- fletimgbtn.py
+++ fletimgbtn.py
@@ -1,3 +1,23 @@
 import flet as ft
+from pathlib import Path
+from flet.core.text_button import OptionalControlEventCallable
+from icon_creater import load_image, image_to_base64
 
 
+class ImageButton(ft.TextButton):
+    def __init__(
+        self,
+        image_path: Path,
+    ):
+        self.__image = ft.Image(src=str(image_path))
+        super().__init__(content=self.__image)
+        # self.on_click = click_handler
+
+    async def change_image_async(self, image_path: Path):
+        image = await load_image(image_path)
+        self.change_image(image_to_base64(image))
+
+    def change_image(self, base64_image: str):
+        self.__image.src_base64 = base64_image
+        if self.page != None:
+            self.page.update()
